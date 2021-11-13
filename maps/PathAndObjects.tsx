39ebc6d3051c8b569adcf8f9ae7d5f8d3796924e
casey shurtliff <shurtliff+imacWannabe@gmail.com>
2021-11-12 20:11:35 -0700

    s

--- maps/PathAndObjects.tsx
+++ maps/PathAndObjects.tsx
@@ -1,4 +1,78 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="PathAndObjects" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="PathAndObjects.png" width="512" height="512"/>
+ <wangsets>
+  <wangset name="stonegrass2" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="3" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="4" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="5" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="19" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="20" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="21" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="35" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="36" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="37" wangid="1,0,0,0,0,0,1,1"/>
+  </wangset>
+  <wangset name="stonegrass1" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="16" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="17" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="18" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="32" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="33" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="34" wangid="1,0,0,0,0,0,1,1"/>
+  </wangset>
+  <wangset name="dirtgrass1" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="6" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="7" wangid="0,0,1,0,1,1,1,0"/>
+   <wangtile tileid="8" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="22" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="23" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="24" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="38" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="39" wangid="0,1,1,0,0,0,1,1"/>
+   <wangtile tileid="40" wangid="1,0,0,0,0,0,1,0"/>
+  </wangset>
+  <wangset name="stonegrass3" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="80" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="81" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="82" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="96" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="97" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="98" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="112" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="113" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="114" wangid="1,0,0,0,0,0,1,1"/>
+  </wangset>
+  <wangset name="stonegrass4" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="83" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="84" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="85" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="99" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="100" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="101" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="115" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="116" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="117" wangid="1,0,0,0,0,0,1,1"/>
+  </wangset>
+  <wangset name="dirtgrass2" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="9" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="10" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="11" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="25" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="26" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="27" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="41" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="42" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="43" wangid="1,0,0,0,0,0,1,1"/>
+  </wangset>
+ </wangsets>
 </tileset>
