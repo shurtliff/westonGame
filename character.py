7419848c7455043b7cac734fce01feec38b5e965
wafflizer <shurtliff@gmail.com>
2021-11-12 21:41:53 -0700

    collistion

--- character.py
+++ character.py
@@ -1,10 +1,11 @@
 import pygame
+from constants import *
 
 
 class Character:
     """Represents a chess piece."""
 
-    def __init__(self, spriteSheet, screen, tiles):
+    def __init__(self, spriteSheet, screen, tiles, mapObject):
         """Initialize attributes to represent a ches piece."""
         self.height = 32
         self.width = 32
@@ -13,24 +14,25 @@ class Character:
         self.tiles = tiles
         self.name = ''
         self.color = ''
+        self.mapObject = mapObject
 
         # Start each piece off at the top left corner.
-        self.x, self.y = 0.0, 0.0
-        self.image = spriteSheet.get_image(self.x, self.y, self.height, self.width)
+        self.x, self.y = mapObject.x, mapObject.y
+        self.image = spriteSheet.get_image(0, 0, self.height, self.width)
 
     def blitme(self):
         """Draw the piece at its current location."""
-        self.rect = self.image.get_rect()
-        self.rect.topleft = self.x, self.y
-        self.screen.blit(self.image, self.rect)
+        # self.rect = self.mapObject.get_rect()
+        # self.rect.topleft = self.x, self.y
+        self.screen.blit(self.image, [(SCREENWIDTH/2), (SCREENHEIGHT/2)])
     def move(self, move):
-        x = self.x+move[0]
-        y = self.y+move[1]
+        x = self.mapObject.x+move[0]
+        y = self.mapObject.y+move[1]
         w = self.width
         h = self.height
         playerrec = pygame.Rect([x,y,w,h])
         check = False
         if (playerrec.collidelistall(self.tiles)): #this tests every tile with the player rectangle
             return
-        self.x = x
-        self.y = y
+        self.mapObject.x = x
+        self.mapObject.y = y
