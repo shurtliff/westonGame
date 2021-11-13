fe67402f9fa51807701ca42a34ee5587d973b829
casey shurtliff <shurtliff+imacWannabe@gmail.com>
2021-11-13 00:05:44 -0700

    remove venv

--- maps/pngs.tsx
+++ maps/pngs.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="pngs" tilewidth="32" tileheight="32" tilecount="1350" columns="45">
+<tileset version="1.5" tiledversion="1.7.2" name="pins" tilewidth="64" tileheight="64" tilecount="330" columns="22">
  <image source="tester.jpg" width="1440" height="960"/>
 </tileset>
